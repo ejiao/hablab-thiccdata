3910d3c2df566f57a375baf49ed84a79b6c40d9e
Yilu Zeng <zengyl.yolanda2015@outlook.com>
2019-04-06 14:57:26 -0400

    menuPage

--- models/menuPage.py
+++ models/menuPage.py
@@ -1,1 +1,16 @@
-#TODO
+from sqlalchemy import Column, Integer, String, TIMESTAMP
+from sqlalchemy.orm import relationship
+
+from . import Base
+# from .participant import Participant
+
+
+class Menu(Base):
+    __tablename__ = 'menu'
+    id = Column(Integer, primary_key=True)
+    menu_id = Column(Integer)
+    page_number = Column(Integer)
+    image_id = Column(Integer)
+    full_height = Column(Integer)
+    full_width = Column(Integer)
+    uuid = Column(UUID)
